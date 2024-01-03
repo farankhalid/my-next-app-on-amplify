f23936cd93af5329c7ddde809ff6aa29a301bd21
Faran Khalid <fkhalid@oneaihealth.com>
2024-01-03 18:19:06 +0500

    my next app on amplify with auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "mynextapponamplify6df3f5c5": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
